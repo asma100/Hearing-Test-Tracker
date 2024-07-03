a9be25aa2521e6fb29b1138c43e5ca2b6c6d58e0
Asma Elhadi <asmaa.alhai100.gmail.com>
2024-07-03 11:31:01 +0300

    editing wsgi

--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,8 @@
-from app import app
+from app import app  # Import the Flask application from app.py
 
-app = app()
+# No need to call app() here, it's already an application object
+
+if __name__ == '__main__':
+    import gunicorn  # Assuming you're using Gunicorn as your WSGI server
+
+    gunicorn.main() 
